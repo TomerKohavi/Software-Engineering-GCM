14b3b11625af55da0cdc89913c5bbbcd1ca4f458
yurixtream <48755374+yurixtream@users.noreply.github.com>
2019-06-09 23:14:10 +0300

    /

---
+++ GCM/src/io_commands/AllCitiesRequest.java
@@ -0,0 +1,12 @@
+package io_commands;
+
+import java.util.ArrayList;
+
+import otherClasses.Pair;
+
+public class AllCitiesRequest extends Command
+{
+	public void delete() {}
+	
+	public ArrayList<Pair<String,Integer>> cityList;
+}
