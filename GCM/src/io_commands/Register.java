2faa118fb98778b759b88ede3bde85e4dcdfae79
liortct <38353773+liortct@users.noreply.github.com>
2019-06-10 14:22:59 +0300

    io commands

--- GCM/src/io_commands/Register.java
+++ GCM/src/io_commands/Register.java
@@ -4,7 +4,24 @@ import objectClasses.User;
 import objectClasses.Employee.Role;
 import server.EchoServer.LoginRegisterResult;
 
+/**
+ * @author sigal
+ * send register request to the server and return the result to the client
+ */
 public class Register extends Command {
+	/**
+	 * @param username the user name of the user
+	 * @param password the password of the user
+	 * @param firstName the firstName of the user
+	 * @param lastName the lastName of the user
+	 * @param email the email of the user
+	 * @param phone the phone of the user
+	 * @param role the role of the employee(if it an employee)
+	 * @param ccard the credit card of the user
+	 * @param expires the expires of the user credit card
+	 * @param cvv the cvv of the user credit card
+	 * @param isEmployee if the user is employee or not
+	 */
 	public Register(String username, String password, String firstName, String lastName, String email, String phone, Role role, String ccard, String expires, String cvv, boolean isEmployee)
 	{
 		this.username = username;
