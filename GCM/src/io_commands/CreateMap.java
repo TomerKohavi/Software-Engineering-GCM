49d50953706aca26c424cc4eb323969b60709b34
yurixtream <48755374+yurixtream@users.noreply.github.com>
2019-06-10 18:58:12 +0300

    map create

---
+++ GCM/src/io_commands/CreateMap.java
@@ -0,0 +1,26 @@
+package io_commands;
+
+import objectClasses.MapSight;
+
+public class CreateMap extends Command
+{
+	public CreateMap(int cityId, String name, String info, String imgURL, int cdvId)
+	{
+		this.cityId = cityId;
+		this.name = name;
+		this.info = info;
+		this.imgURL = imgURL;
+		this.cdvId = cdvId;
+	}
+	
+	public void delete() {
+		this.cityId = null;
+		this.name = null;
+		this.info = null;
+		this.imgURL = null;
+	}
+	
+	public Integer cityId, cdvId;
+	public String name, info, imgURL;
+	public MapSight mapS;
+}
