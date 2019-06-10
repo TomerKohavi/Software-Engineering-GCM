93dbb06fb9b37c4157f837dd643cf692fccf5f8b
Ron Cohen <RonCohen555@users.noreply.github.com>
2019-06-10 09:14:16 +0300

    small fix

--- GCM/src/objectClasses/OneTimePurchase.java
+++ GCM/src/objectClasses/OneTimePurchase.java
@@ -19,7 +19,7 @@ public class OneTimePurchase extends CityPurchase implements ClassMustProperties
         return new OneTimePurchase(id,cityId,userId,purchaseDate, fullPrice,pricePayed,wasDownload);
     }
 
-    public OneTimePurchase(User u,City c, Date purchaseDate, double fullPrice, double pricePayed) {
+    public OneTimePurchase(Customer u,City c, Date purchaseDate, double fullPrice, double pricePayed) {
         super(u.getId(), c.getId(), purchaseDate, fullPrice, pricePayed);
         this.wasDownload = false;
     }
