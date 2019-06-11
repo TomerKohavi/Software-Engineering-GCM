be7a9d444bb6cf2e341030af17300c27c6a0d957
Ron Cohen <RonCohen555@users.noreply.github.com>
2019-06-11 11:19:48 +0300

    fix num months

--- GCM/src/objectClasses/Subscription.java
+++ GCM/src/objectClasses/Subscription.java
@@ -13,12 +13,15 @@ public class Subscription extends CityPurchase implements ClassMustProperties, S
 	@SuppressWarnings("deprecation")
 	public static final Time closeTime = new Time(3 * 24, 0, 0);
 	private Date expirationDate;
-	public int months = getNumMonths();
+	
+	
+	public int numMonths = -1; //temp_variable
 
 	private Subscription(int id, int cityId, int userId, Date purchaseDate, double fullPrice, double pricePayed,
 			Date expirationDate) {
 		super(id, cityId, userId, purchaseDate, fullPrice, pricePayed);
 		this.expirationDate = expirationDate;
+		this.numMonths = calcNumMonths();
 	}
 
 	public static Subscription _createSubscription(int id, int cityId, int userId, Date purchaseDate, double fullPrice,
@@ -29,6 +32,7 @@ public class Subscription extends CityPurchase implements ClassMustProperties, S
 	public Subscription(Customer u, int cityId, Date purchaseDate, double fullPrice, double pricePayed, Date expirationDate) {
 		super(u.getId(), cityId, purchaseDate, fullPrice, pricePayed);
 		this.expirationDate = expirationDate;
+		this.numMonths = calcNumMonths();
 	}
 
 	public void saveToDatabase() {
@@ -49,15 +53,28 @@ public class Subscription extends CityPurchase implements ClassMustProperties, S
 
 	public void setExpirationDate(Date expirationDate) {
 		this.expirationDate = expirationDate;
+		this.numMonths = calcNumMonths();
+	}
+	
+	private int calcNumMonths() {
+		 int months = (this.expirationDate.getYear()-super.getPurchaseDate().getYear())*12+this.expirationDate.getMonth()-super.getPurchaseDate().getMonth();
+		 return months;
 	}
 	
 	public int getNumMonths() {
-		 months = (this.expirationDate.getYear()-super.getPurchaseDate().getYear())*12+this.expirationDate.getMonth()-super.getPurchaseDate().getMonth();
-		 return months;
+		return this.numMonths;
 	}
 
 	public void setNumMonths(int _months) {
-		 months = _months;
+		 this.numMonths = _months;
+		 Date pd=super.getPurchaseDate();
+		 int newYear=pd.getYear()+_months/12;
+		 int newMonth=pd.getMonth()+_months%12;
+		 if(newMonth>12) {
+			 newYear++;
+			 newMonth=newMonth%12;
+		 }
+		 this.expirationDate=new Date(newYear,newMonth,pd.getDay());
 	}
 	
 	@Override
