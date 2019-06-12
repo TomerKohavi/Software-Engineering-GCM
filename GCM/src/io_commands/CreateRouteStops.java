d38731d646b6157172c1ca9aa4e79c17ea5a9d50
liortct <38353773+liortct@users.noreply.github.com>
2019-06-12 15:03:13 +0300

    client java doc

--- GCM/src/io_commands/CreateRouteStops.java
+++ GCM/src/io_commands/CreateRouteStops.java
@@ -4,9 +4,16 @@ import java.util.ArrayList;
 
 import objectClasses.RouteStop;
 
+/**
+ * @author sigal
+ * treat create route stops request from the client to the server
+ */
 public class CreateRouteStops extends Command
 {
 	
+	/**
+	 * @param stopList list of route stop to handle
+	 */
 	public CreateRouteStops(ArrayList<RouteStop> stopList)
 	{
 		this.stopList = stopList;
