679741f22385a18362565ea851e01bab50619406
liortct <38353773+liortct@users.noreply.github.com>
2019-06-13 16:56:53 +0300

    Customer javaDoc

--- GCM/src/objectClasses/Customer.java
+++ GCM/src/objectClasses/Customer.java
@@ -8,6 +8,10 @@ import java.util.Calendar;
 import controller.Database;
 import otherClasses.ClassMustProperties;
 
+/**
+ * @author ron
+ * TODO: fix
+ */
 @SuppressWarnings("serial")
 public class Customer extends User implements ClassMustProperties, Serializable {
 	
@@ -21,6 +25,20 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 	ArrayList<Subscription> temp_removeSubscription;
 	ArrayList<OneTimePurchase> temp_removeOneTimePurchase;
 
+	/**
+	 * This is a private constructor of customer object
+	 * 
+	 * @param id the customer id
+	 * @param userName the customer user name
+	 * @param password the customer password
+	 * @param email the customer email
+	 * @param firstName the customer first name
+	 * @param lastName the customer last name
+	 * @param phoneNumber the customer phone number
+	 * @param creditCardNum the customer credit card number
+	 * @param creditCardExpires the customer credit card expires time
+	 * @param cvc the customer credit card cvc
+	 */
 	private Customer(int id, String userName, String password, String email, String firstName, String lastName,
 			String phoneNumber,String creditCardNum,String creditCardExpires,String cvc) {
 		super(id, userName, password, email, firstName, lastName, phoneNumber);
@@ -30,11 +48,40 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 		reloadTempsFromDatabase();
 	}
 
+	/**
+	 * This function create customer object according to all the inputs (supposed to be
+	 * used only in Database)
+	 * 
+	 * @param id the customer id
+	 * @param userName the customer user name
+	 * @param password the customer password
+	 * @param email the customer email
+	 * @param firstName the customer first name
+	 * @param lastName the customer last name
+	 * @param phoneNumber the customer phone number
+	 * @param creditCardNum the customer credit card number
+	 * @param creditCardExpires the customer credit card expires time
+	 * @param cvc the customer credit card cvc
+	 * @return the new customer object
+	 */
 	public static Customer _createCustomer(int id, String userName, String password, String email, String firstName,
 			String lastName, String phoneNumber,String creditCardNum,String creditCardExpires,String cvc) { // friend to Database
 		return new Customer(id, userName, password, email, firstName, lastName, phoneNumber,creditCardNum,creditCardExpires,cvc);
 	}
 
+	/**
+	 * This is the normal public constructor for City object
+	 * 
+	 * @param userName the customer user name
+	 * @param password the customer password
+	 * @param email the customer email
+	 * @param firstName the customer first name
+	 * @param lastName the customer last name
+	 * @param phoneNumber the customer phone number
+	 * @param creditCardNum the customer credit card number
+	 * @param creditCardExpires the customer credit card expires time
+	 * @param cvc the customer credit card cvc
+	 */
 	public Customer(String userName, String password, String email, String firstName, String lastName,
 			String phoneNumber,String creditCardNum,String creditCardExpires,String cvc) {
 		super(userName, password, email, firstName, lastName, phoneNumber);
@@ -57,16 +104,31 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 		this.temp_removeOneTimePurchase = new ArrayList<>();
 	}
 
+	/**
+	 * generate active subscriptions from given date
+	 * @param dateToCheck from which date to generate the active subscriptions
+	 * @return list of active subscriptions 
+	 */
 	private ArrayList<Subscription> generateActiveSubscriptions(Date dateToCheck) {
 		ArrayList<Integer> ids = Database.searchSubscription(super.getId(), null, dateToCheck, true);
 		return generateListSubscriptions(ids);
 	}
 
+	/**
+	 * generate unactive subscriptions from given date
+	 * @param dateToCheck from which date to generate the unactive subscriptions
+	 * @return list of unactive subscriptions 
+	 */
 	private ArrayList<Subscription> generateUnactiveSubscriptions(Date dateToCheck) {
 		ArrayList<Integer> ids = Database.searchSubscription(super.getId(), null, dateToCheck, false);
 		return generateListSubscriptions(ids);
 	}
 
+	/**
+	 * generate list of subscriptions from given list of ids
+	 * @param ids ids of the subscriptions we want to get
+	 * @return list of subscriptions objects
+	 */
 	private ArrayList<Subscription> generateListSubscriptions(ArrayList<Integer> ids) {
 		ArrayList<Subscription> arrList = new ArrayList<Subscription>();
 		for (int id : ids) {
@@ -78,11 +140,20 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 		return arrList;
 	}
 
+	/**
+	 * Returns the customer one time purchase lise
+	 * @return list of one time purchase object of the customer
+	 */
 	private ArrayList<OneTimePurchase> generateOneTimePurchases() {
 		ArrayList<Integer> ids = Database.searchOneTimePurchase(super.getId(), null, null, null);
 		return generateListOneTimePurchases(ids);
 	}
 
+	/**
+	 * generate list of one time purchase from given list of ids
+	 * @param ids ids of the one time purchase we want to get
+	 * @return list of one time purchase objects
+	 */
 	private ArrayList<OneTimePurchase> generateListOneTimePurchases(ArrayList<Integer> ids) {
 		ArrayList<OneTimePurchase> arrList = new ArrayList<OneTimePurchase>();
 		for (int id : ids) {
@@ -132,6 +203,10 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 			otp.deleteFromDatabase();
 	}
 
+	/**
+	 * @param sub subscription object to add to the customer
+	 * @return if the subscription is added successfully
+	 */
 	public boolean addSubscription(Subscription sub) {
 		if (sub.getUserId() != this.getId())
 			return false;
@@ -143,6 +218,10 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 		return true;
 	}
 
+	/**
+	 * @param otp one time purchase object to add to the customer
+	 * @return if the one time purchase is added successfully
+	 */
 	public boolean addOneTimePurchase(OneTimePurchase otp) {
 		if (otp.getUserId() != this.getId())
 			return false;
@@ -150,6 +229,10 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 		return true;
 	}
 
+	/**
+	 * Returns list of subscription that is going to be expired
+	 * @return list of subscription that is going to be expired
+	 */
 	public ArrayList<Subscription> getGoingToEnd() {
 		ArrayList<Subscription> result = new ArrayList<>();
 		Date today = new Date(Calendar.getInstance().getTime().getTime());
@@ -159,6 +242,10 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 		return result;
 	}
 
+	/** TODO: ron fill it
+	 * @param cityId
+	 * @return
+	 */
 	public boolean canViewCityWithSubscription(int cityId) {
 		for (Subscription s : temp_activeSubscription)
 			if (s.getCityId() == cityId)
@@ -166,6 +253,11 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 		return false;
 	}
 
+	/**
+	 * get the active one time purchase in the city
+	 * @param cityId the city id
+	 * @return one time purchase object that active in the city
+	 */
 	public OneTimePurchase getActiveOneTimePurchaseByCity(int cityId) {
 		for (OneTimePurchase otp : temp_oneTimePurchase)
 			if (otp.getCityId() == cityId && !otp.getWasDownload())
@@ -173,6 +265,11 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 		return null;
 	}
 
+	/**
+	 * remove subscription with given id
+	 * @param subscriptionId the id of the subscription we want to remove 
+	 * @return the subscription object that we remove
+	 */
 	public Subscription removeSubscription(int subscriptionId) {
 		for (Subscription s : new ArrayList<>(temp_activeSubscription)) {
 			if (s.getId() == subscriptionId) {
@@ -191,6 +288,11 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 		return null;
 	}
 
+	/**
+	 * get subscription object from given id
+	 * @param subscriptionId the id of the subscription we want to get
+	 * @return subscription object we wanted
+	 */
 	public Subscription getSubscription(int subscriptionId) {
 		for (Subscription s : temp_activeSubscription)
 			if (s.getId() == subscriptionId)
@@ -201,6 +303,11 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 		return null;
 	}
 
+	/**
+	 * get the one time purchase by given id subscription
+	 * @param subscriptionId the subscription id  
+	 * @return one time purchase object that contains the subscription  
+	 */
 	public OneTimePurchase getOneTimePurchase(int subscriptionId) {
 		for (OneTimePurchase otp : temp_oneTimePurchase)
 			if (otp.getId() == subscriptionId)
@@ -208,6 +315,11 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 		return null;
 	}
 
+	/**
+	 * remove one time purchase by given id
+	 * @param otpId one time purchase id to remove
+	 * @return the one time purchase that removed
+	 */
 	public OneTimePurchase removeOneTimePurchase(int otpId) {
 		for (OneTimePurchase otp : new ArrayList<>(temp_oneTimePurchase)) {
 			if (otp.getId() == otpId) {
@@ -219,38 +331,80 @@ public class Customer extends User implements ClassMustProperties, Serializable 
 		return null;
 	}
 
+	/**
+	 * returns list of copied active subscriptions
+	 * @return list of copied active subscriptions
+	 */
 	public ArrayList<Subscription> getCopyActiveSubscription() {
 		return new ArrayList<>(temp_activeSubscription);
 	}
 
+	/**
+	 * returns list of copied unactive subscriptions
+	 * @return list of copied unactive subscriptions
+	 */
 	public ArrayList<Subscription> getCopyUnactiveSubscription() {
 		return new ArrayList<>(temp_unactiveSubscription);
 	}
 
+	/**
+	 * returns list of copied one time purchase
+	 * @return list of copied one time purchase
+	 */
 	public ArrayList<OneTimePurchase> getCopyOneTimePurchase() {
 		return new ArrayList<>(temp_oneTimePurchase);
 	}
 
+	/**
+	 * Return credit card number
+	 * 
+	 * @return credit card number
+	 */
 	public String getCreditCardNum() {
 		return creditCardNum;
 	}
 
+	/**
+	 * Sets the credit card number
+	 * 
+	 * @param creditCardNum the new credit card number
+	 */
 	public void setCreditCardNum(String creditCardNum) {
 		this.creditCardNum = creditCardNum;
 	}
 
+	/**
+	 * Return credit card expired date
+	 * 
+	 * @return credit card expired date
+	 */
 	public String getCreditCardExpires() {
 		return creditCardExpires;
 	}
 
+	/**
+	 * Sets the credit card expired date
+	 * 
+	 * @param creditCardExpires the new credit card expired date
+	 */
 	public void setCreditCardExpires(String creditCardExpires) {
 		this.creditCardExpires = creditCardExpires;
 	}
 
+	/**
+	 * Return the credit card cvc
+	 * 
+	 * @return the credit card cvc
+	 */
 	public String getCvc() {
 		return cvc;
 	}
 
+	/**
+	 * Sets the credit card cvc
+	 * 
+	 * @param cvc the new credit card cvc
+	 */
 	public void setCvc(String cvc) {
 		this.cvc = cvc;
 	}
