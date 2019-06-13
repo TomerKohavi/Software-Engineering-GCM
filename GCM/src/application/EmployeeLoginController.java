95798fe800bdc5aad4794fac70d5ed216c3e6024
liortct <38353773+liortct@users.noreply.github.com>
2019-06-13 10:24:24 +0300

    EmployeeLoginController javaDoc

--- GCM/src/application/EmployeeLoginController.java
+++ GCM/src/application/EmployeeLoginController.java
@@ -21,6 +21,10 @@ import objectClasses.User;
 import otherClasses.Pair;
 import server.EchoServer.LoginRegisterResult;
 
+/**
+ * @author tomer
+ * controller for employee to connect UI to client
+ */
 public class EmployeeLoginController
 {
 
@@ -48,42 +52,71 @@ public class EmployeeLoginController
 	@FXML // fx:id="RegisterHL"
 	private Hyperlink RegisterHL; // Value injected by FXMLLoader
 
+	/**
+	 * @param FXMLpage the fxml page
+	 * @throws IOException if there is a error in load the fxml
+	 */
 	void loadPage(String FXMLpage) throws IOException
 	{
 		AnchorPane pane = (AnchorPane) FXMLLoader.load((URL) this.getClass().getResource(FXMLpage));
 		mainPane.getChildren().setAll(pane);
 	}
 
+    /**
+     * go to the previous page
+     * @param event user click go previous page
+     */
 	@FXML
 	void goBack(ActionEvent event) throws IOException
 	{
 		loadPage("HomePageScene.fxml");
 	}
 
+	
+	/**
+	 * @param event the customer ask for access to page
+	 * @throws IOException cannot load page
+	 */
 	@FXML
 	void customerAccess(ActionEvent event) throws IOException
 	{
 		loadPage("LoginScene.fxml");
 	}
 
+	/**
+	 * @param event user click on register
+	 * @throws IOException cannot loag the register page
+	 */
 	@FXML
 	void register(ActionEvent event) throws IOException
 	{
 		loadPage("employeeRegisterScene.fxml");
 	}
 
+	/**
+	 * get user passward
+	 * @param event user click on get password button
+	 */
 	@FXML
 	void getPassword(ActionEvent event)
 	{
 		System.out.println(Password.getText());
 	}
 
+	/**
+	 * get user name
+	 * @param event user click on get user name button
+	 */
 	@FXML
 	void getUsername(ActionEvent event)
 	{
 		System.out.println(Username.getText());
 	}
 
+	/**
+	 * @param event user click on login
+	 * @throws IOException cannot login
+	 */
 	@FXML
 	void login(ActionEvent event) throws IOException
 	{
