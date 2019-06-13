af4ac5880d68b0695f3132da69c79b19d5292e8b
liortct <38353773+liortct@users.noreply.github.com>
2019-06-13 17:27:01 +0300

    location java doc

--- GCM/src/objectClasses/Location.java
+++ GCM/src/objectClasses/Location.java
@@ -5,6 +5,10 @@ import java.io.Serializable;
 import controller.Database;
 import otherClasses.ClassMustProperties;
 
+/**
+ * @author ron
+ * TODO: fix
+ */
 @SuppressWarnings("serial")
 public class Location implements ClassMustProperties, Serializable {
 	private int id;
@@ -14,6 +18,14 @@ public class Location implements ClassMustProperties, Serializable {
 
 	private PlaceOfInterest temp_place;
 
+	/**
+	 * This is a private constructor of location object
+	 * 
+	 * @param id the id of the location
+	 * @param mapId the map id that contains the location
+	 * @param placeOfInterestId the place of interest id that contains the location
+	 * @param coordinates the coordinates of the location  
+	 */
 	private Location(int id, int mapId, int placeOfInterestId, double[] coordinates) {
 		this.id = id;
 		this.mapId = mapId;
@@ -22,12 +34,29 @@ public class Location implements ClassMustProperties, Serializable {
 		reloadTempsFromDatabase();
 	}
 
+	/**
+	 * This function create location object according to all the inputs (supposed to be
+	 * used only in Database)
+	 * 
+	 * @param id the id of the location
+	 * @param mapId the map id that contains the location
+	 * @param placeOfInterestId the place of interest id that contains the location
+	 * @param coordinates the coordinates of the location  
+	 * @return the new location object
+	 */
 	public static Location _createLocation(int id, int mapId, int placeOfInterestId, double[] coordinates) { // friend
 																												// to
 																												// Database
 		return new Location(id, mapId, placeOfInterestId, coordinates);
 	}
 
+	/**
+	 * Create location by given map, point of interest and coordinates
+	 * 
+	 * @param m the map 
+	 * @param p the point of interest
+	 * @param coordinates the coordinates of the location
+	 */
 	public Location(Map m, PlaceOfInterest p, double[] coordinates) {
 		this.id = Database.generateIdLocation();
 		this.mapId = m.getId();
@@ -48,26 +77,56 @@ public class Location implements ClassMustProperties, Serializable {
 		this.temp_place = Database.getPlaceOfInterestById(this.placeOfInterestId);
 	}
 
+	/**
+	 * Return the location id
+	 * 
+	 * @return the location id
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Return the map id
+	 * 
+	 * @return the map id
+	 */
 	public int getMapId() {
 		return mapId;
 	}
 
+	/**
+	 * Return the place of interest id
+	 * 
+	 * @return the place of interest id
+	 */
 	public int getPlaceOfInterestId() {
 		return placeOfInterestId;
 	}
-
+	
+	/**
+	 * Return the coordinates of the location
+	 * 
+	 * @return the coordinates of the location
+	 */
 	public double[] getCoordinates() {
 		return coordinates;
 	}
 
+	/**
+	 * Return a copied place of interest
+	 * 
+	 * @return copied place of interest
+	 */
 	public PlaceOfInterest getCopyPlaceOfInterest() {
 		return temp_place;
 	}
 
+	/**
+	 * Sets the coordinates of the location
+	 * 
+	 * @param coordinates the new coordinates of the location
+	 */
 	public void setCoordinates(double[] coordinates) {
 		this.coordinates = coordinates;
 	}
