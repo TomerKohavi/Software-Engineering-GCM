8957546c7a75246ac099b58e051c8c0ce47e681e
Ron Cohen <RonCohen555@users.noreply.github.com>
2019-06-14 13:44:08 +0300

    information system java doc

--- GCM/src/controller/InformationSystem.java
+++ GCM/src/controller/InformationSystem.java
@@ -10,6 +10,11 @@ import java.sql.Date;
 import java.util.Calendar;
 import otherClasses.Pair;
 
+/**
+ * Controller class that handle the managment of statistics
+ * @author Ron Cohen
+ *
+ */
 public final class InformationSystem
 {
 	
@@ -38,7 +43,7 @@ public final class InformationSystem
 	 */
 	public static Statistic getStatistic(int cityId, Date d)
 	{
-		ArrayList<Integer> ids = Database.searchStatistic((Integer) cityId, d, null, null, null);
+		ArrayList<Integer> ids = Database.searchStatistic((Integer) cityId, d, null, null, null,null);
 		if (ids.size() != 1)
 			return null;
 		Statistic s = Database._getStatisticById(ids.get(0));
@@ -82,6 +87,13 @@ public final class InformationSystem
 		return sum;
 	}
 
+	/**
+	 * Calc and return a detailed list of the changes to num maps in the date range
+	 * @param cityId the city id, can be null to request all cities id
+	 * @param from date range from
+	 * @param end date range to
+	 * @return list of the num maps and the date range it was relevant
+	 */
 	public static ArrayList<Pair<Pair<Date, Date>, Integer>> getRangeNumMaps(Integer cityId, Date from, Date end)
 	{
 		ArrayList<Integer> ids = Database.searchStatistic((Integer) cityId, null, null, end, null,true);
@@ -121,6 +133,11 @@ public final class InformationSystem
 		return ans;
 	}
 
+	/**
+	 * Sums the values of java.util.Map
+	 * @param map the map
+	 * @return the sum of the values in the map
+	 */
 	private static int sumMapList(java.util.Map<Integer, Integer> map)
 	{
 		int sum = 0;
@@ -131,11 +148,20 @@ public final class InformationSystem
 		return sum;
 	}
 
+	/**
+	 * Adds an one time purchase to the statistic of the city id and today date
+	 * @param cityId the city id
+	 */
 	public static void addOneTimePurchase(int cityId)
 	{
 		addOneTimePurchase(cityId, new Date(Calendar.getInstance().getTime().getTime()));
 	}
 
+	/**
+	 * Adds an one time purchase to the statistic of the city id and the date
+	 * @param cityId the city id 
+	 * @param d the date
+	 */
 	public static void addOneTimePurchase(int cityId, Date d)
 	{
 		Statistic s = getStatistic(cityId, d);
@@ -145,11 +171,22 @@ public final class InformationSystem
 		s.saveToDatabase();
 	}
 
+	/**
+	 * Set the n number maps of the statistic with city id and today date
+	 * @param cityId the city id
+	 * @param numMaps the number of maps
+	 */
 	public static void setNumMaps(int cityId, int numMaps)
 	{
 		setNumMaps(cityId, numMaps, new Date(Calendar.getInstance().getTime().getTime()));
 	}
 
+	/**
+	 * Set the n number maps of the statistic with city id and the date
+	 * @param cityId the city id
+	 * @param d the date
+	 * @param numMaps the number of maps
+	 */
 	public static void setNumMaps(int cityId, int numMaps, Date d)
 	{
 		Statistic s = getStatistic(cityId, d);
@@ -159,11 +196,20 @@ public final class InformationSystem
 		s.saveToDatabase();
 	}
 
+	/**
+	 * Adds a visit to the statistic of the city id and today date
+	 * @param cityId the city id
+	 */
 	public static void addVisit(int cityId)
 	{
 		addVisit(cityId, new Date(Calendar.getInstance().getTime().getTime()));
 	}
 
+	/**
+	 * Adds a visit to the statistic of the city id and the date
+	 * @param cityId the city id 
+	 * @param d the date
+	 */
 	public static void addVisit(int cityId, Date d)
 	{
 		Statistic s = getStatistic(cityId, d);
@@ -173,11 +219,20 @@ public final class InformationSystem
 		s.saveToDatabase();
 	}
 
+	/**
+	 * Adds a subscription purchase to the statistic of the city id and today date
+	 * @param cityId the city id
+	 */
 	public static void addSubscription(int cityId)
 	{
 		addSubscription(cityId, new Date(Calendar.getInstance().getTime().getTime()));
 	}
 
+	/**
+	 * Adds a subscription purchase to the statistic of the city id and the date
+	 * @param cityId the city id 
+	 * @param d the date
+	 */
 	public static void addSubscription(int cityId, Date d)
 	{
 		Statistic s = getStatistic(cityId, d);
@@ -187,11 +242,20 @@ public final class InformationSystem
 		s.saveToDatabase();
 	}
 
+	/**
+	 * Adds a subscription renewal purchase to the statistic of the city id and today date
+	 * @param cityId the city id
+	 */
 	public static void addSubscriptionRenewal(int cityId)
 	{
 		addSubscriptionRenewal(cityId, new Date(Calendar.getInstance().getTime().getTime()));
 	}
 
+	/**
+	 * Adds a subscription renewal purchase to the statistic of the city id and the date
+	 * @param cityId the city id 
+	 * @param d the date
+	 */
 	public static void addSubscriptionRenewal(int cityId, Date d)
 	{
 		Statistic s = getStatistic(cityId, d);
@@ -201,11 +265,20 @@ public final class InformationSystem
 		s.saveToDatabase();
 	}
 
+	/**
+	 * Adds a subscription download to the statistic of the city id and today date
+	 * @param cityId the city id
+	 */
 	public static void addSubDownload(int cityId)
 	{
 		addSubDownload(cityId, new Date(Calendar.getInstance().getTime().getTime()));
 	}
 
+	/**
+	 * Adds a subscription download to the statistic of the city id and the date
+	 * @param cityId the city id 
+	 * @param d the date
+	 */
 	public static void addSubDownload(int cityId, Date d)
 	{
 		Statistic s = getStatistic(cityId, d);
@@ -215,11 +288,19 @@ public final class InformationSystem
 		s.saveToDatabase();
 	}
 
+	/**
+	 * Updates the statistic of the city id that a new version of this city was published today
+	 * @param cityId the city id
+	 */
 	public static void newVersionWasPublished(int cityId)
 	{
 		newVersionWasPublished(cityId, new Date(Calendar.getInstance().getTime().getTime()));
 	}
 
+	/**
+	 * Updates the statistic of the city id that a new version of this city was published in the date
+	 * @param cityId the city id
+	 */
 	public static void newVersionWasPublished(int cityId, Date d)
 	{
 		Statistic s = getStatistic(cityId, d);
@@ -229,19 +310,32 @@ public final class InformationSystem
 		s.saveToDatabase();
 	}
 
+	/**
+	 * Returns list of the statistics id that with today date and describe cities that has new published version
+	 * @return list of the statistics ids
+	 */
 	public static ArrayList<Integer> getCitiesWithNewPublishedVersion()
 	{
 		return getCitiesWithNewPublishedVersion(new Date(Calendar.getInstance().getTime().getTime()));
 	}
 
+	/**
+	 * Returns list of the statistics id that with the date and describe cities that has new published version
+	 * @param d the date of the statistics
+	 * @return list of the statistics ids
+	 */
 	public static ArrayList<Integer> getCitiesWithNewPublishedVersion(Date d)
 	{
-		return Database.searchStatistic(null, d, null, null, true);
+		return Database.searchStatistic(null, d, null, null, true,null);
 	}
 
+	/**
+	 * Return a list of all the statistics
+	 * @return list of statisics
+	 */
 	public static ArrayList<Statistic> getAllStatistics()
 	{
-		ArrayList<Integer> ids = Database.searchStatistic(null, null, null, null, null);
+		ArrayList<Integer> ids = Database.searchStatistic(null, null, null, null, null,null);
 		ArrayList<Statistic> arrList = new ArrayList<Statistic>();
 		for (int id : ids)
 		{
