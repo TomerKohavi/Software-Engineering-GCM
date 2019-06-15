ce7938b126df871ade8112259c13988ea740e0dc
yurixtream <48755374+yurixtream@users.noreply.github.com>
2019-06-16 00:53:48 +0300

    w

--- GCM/src/otherClasses/ClassMustProperties.java
+++ GCM/src/otherClasses/ClassMustProperties.java
@@ -1,28 +1,35 @@
 package otherClasses;
 
 import java.io.Serializable;
+import java.sql.SQLException;
 
 /**
  * Interface that describes is necessary for object classes to have
- * @author Ron Cohen
+ * @author Ronen Cohen
  *
  */
 public interface ClassMustProperties extends Serializable {
 	
 	/**
 	 * Function that saves the object to the database
+	 * 
+	 * @throws SQLException if database access failed
 	 */
-	public void saveToDatabase();
+	public void saveToDatabase() throws SQLException;
 
 	/**
 	 * Function that delete the object from the database
+	 * 
+	 * @throws SQLException if database access failed
 	 */
-	public void deleteFromDatabase();
+	public void deleteFromDatabase() throws SQLException;
 
 	/**
 	 * Function that reloads the object temps variables from the database
+	 * 
+	 * @throws SQLException if database access failed
 	 */
-	public void reloadTempsFromDatabase();
+	public void reloadTempsFromDatabase() throws SQLException;
 	
 	/**
 	 * Function that returns the id of the object
